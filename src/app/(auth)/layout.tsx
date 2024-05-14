14e2eb057991e78ecf501c73f518c9a6d32d541d
root <root@DESKTOP-I7KCQJ7>
2024-05-14 19:50:20 +0100

    added layout and sidebar

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from "next";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+  <main>
+  </main>
+  );
+}
