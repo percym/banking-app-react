54ea974e4e3d40e3cbcc126b23a1a29cae8fc7d1
root <root@DESKTOP-I7KCQJ7>
2024-05-15 09:05:36 +0100

    added welcome component

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn={firstName:'Adrian'};
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+               <HeaderBox 
+                  type="greeting"
+                  title="welcome"
+                  user={loggedIn?.firstName|| 'Guest'}
+                  subtext="Track and manage your accounts efficiently."/>
+            </header>
+        </div>
+    </section>
   )
 }
 
