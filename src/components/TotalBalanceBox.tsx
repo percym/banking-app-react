d223622a7c0fbc3a48d72426086c85cb1dcfc163
root <root@DESKTOP-I7KCQJ7>
2024-05-15 09:34:29 +0100

    changed compents to functional

---
+++ src/components/TotalBalanceBox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TotalBalanceBox({
+    accounts=[],totalBanks,totalCurrentBalance}:TotalBalanceBoxProps) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TotalBalanceBox
