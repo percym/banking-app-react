324837db60d02e2114fd33185548fd8c4f67ea8a
root <root@DESKTOP-I7KCQJ7>
2024-05-17 14:57:31 +0100

    updated logo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import path from 'path'
 import React from 'react'
 import { usePathname } from 'next/navigation'
+import logo from '../public/icons/logo.svg'
 
 const SideBar = ({user}:SiderbarProps) => {
   const pathname = usePathname();
@@ -13,9 +14,9 @@ const SideBar = ({user}:SiderbarProps) => {
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
             <Link href="/"
-                className="mb-12 cursor-pointer items-center gap-2">
+                className="mb-12 cursor-pointer items-center gap-2 flex">
                     <Image 
-                        src="/icons/logo.svg"
+                        src={logo}
                         width={34}
                         height={34}
                         alt="logo"
@@ -29,7 +30,14 @@ const SideBar = ({user}:SiderbarProps) => {
                       href={item.route} 
                       key={item.label}
                       className={cn('sidebar-link',{'bg-bank-gradient':isActive})}>
-                        {item.label}
+                        <div className="relative size-6">	
+                          <Image 
+                            src={item.imgURL} 
+                            alt={item.label}
+                            fill
+                            className={cn({'brightness-[3] invert-0 ':isActive})} />
+                          <p className={cn('sidebar-label',{'!text-white':isActive})}>{item.label}</p>
+                        </div>
                   </Link>
               )
             })}
